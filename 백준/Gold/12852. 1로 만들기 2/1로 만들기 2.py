62bcb5f6e8b39d06524af875501dcc7eb6b220c7
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-24 22:44:22 +0900

    [Gold V] Title: 1로 만들기 2, Time: 132 ms, Memory: 126392 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -4,36 +4,26 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [[sys.maxsize, [1]] for _ in range(n + 1)]
-dp[1] = [0, [1]]
+dp = [sys.maxsize for _ in range(n + 1)]
+prev = [0 for _ in range(n + 1)]
+dp[1] = 0
 
 for current_number in range(2, n + 1):
-    if (current_number % 3 == 0):
-        prev_number = current_number // 3
-        prev_cnt, prev_process = dp[prev_number]
-        current_cnt, current_process = dp[current_number]
-        if (prev_cnt + 1 < current_cnt):
-            current_process = prev_process[:]
-            current_process.append(current_number)
-            dp[current_number] = [prev_cnt + 1, current_process]
-    if (current_number % 2 == 0):
-        prev_number = current_number // 2
-        prev_cnt, prev_process = dp[prev_number]
-        current_cnt, current_process = dp[current_number]
-        if (prev_cnt + 1 < current_cnt):
-            current_process = prev_process[:]
-            current_process.append(current_number)
-            dp[current_number] = [prev_cnt + 1, current_process]
-    prev_number = current_number - 1
-    prev_cnt, prev_process = dp[prev_number]
-    current_cnt, current_process = dp[current_number]
-    if (prev_cnt + 1 < current_cnt):
-        current_process = prev_process[:]
-        current_process.append(current_number)
-        dp[current_number] = [prev_cnt + 1, current_process]
-    
-print(dp[n][0])
-print(" ".join(map(str, dp[n][1][::-1])))
+    dp[current_number] = dp[current_number - 1] + 1
+    prev[current_number] = current_number - 1
+    if (current_number % 2 == 0 and dp[current_number // 2] + 1 < dp[current_number]):
+        dp[current_number] = dp[current_number // 2] + 1
+        prev[current_number] = current_number // 2
+    if (current_number % 3 == 0 and dp[current_number // 3] + 1 < dp[current_number]):
+        dp[current_number] = dp[current_number // 3] + 1
+        prev[current_number] = current_number // 3
 
-        
 
+path = []
+node = n
+while(node != 0):
+    path.append(node)
+    node = prev[node]
+
+print(dp[n])
+print(" ".join(map(str, path)))
