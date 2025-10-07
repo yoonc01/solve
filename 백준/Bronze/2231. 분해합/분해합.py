a55e90a28c40b543cff0ba9bcd36d4ea3d6cd13d
윤효준 😎 <143938662+yoonc01@users.noreply.github.com>
2025-10-07 15:46:34 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,16 +1,11 @@
 n = int(input())
 
-is_exist = False
-for i in range(n):
-    string_i = str(i)
-    sum_val = i
-    for num in string_i:
-        sum_val = sum_val + int(num)
-        
-    if sum_val == n:
-        print(i)
-        is_exist = True
+start = max(0, n - 9 * len(str(n)))
+result = 0
+
+for i in range(start, n):
+    if i + sum(map(int, str(i))) == n:
+        result = i
         break
 
-if not is_exist:
-    print(0)
+print(result)
