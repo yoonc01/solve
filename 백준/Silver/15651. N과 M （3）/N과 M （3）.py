749f79d2fcbbb3ad839bf4ff9c9de4077fff4269
윤효준 😎 <143938662+yoonc01@users.noreply.github.com>
2025-10-09 20:32:59 +0900

    [Silver III] Title: N과 M (3), Time: 648 ms, Memory: 149500 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+arr = []
+
+def backtrack():
+    if len(arr) == m:
+        print(*arr)
+        return
+    for i in range(1, n + 1):
+        arr.append(i)
+        backtrack()
+        arr.pop()
+        
+backtrack()
