caf5af435abf016b506f53b4ecc7e25c7e51d1aa
윤효준 😎 <143938662+yoonc01@users.noreply.github.com>
2025-10-09 21:18:35 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,12 +1,9 @@
 n = int(input())
 
-ans = [0, n - 2]
+arr = [0 for i in range(n + 1)]
+arr[1] = 1
+arr[2] = 1
+for i in range(3, n + 1):
+    arr[i] = arr[i - 1] + arr[i - 2]
 
-def fib1(n):
-    if n == 1 or n == 2:
-        ans[0] = ans[0] + 1
-        return 1
-    return fib1(n - 1) + fib1(n - 2)
-
-fib1(n)
-print(*ans)
+print(arr[n], n - 2)
