5045b6aa57d8b06b5d49cfb05a598f7ebd90bfd2
윤효준 😎 <143938662+yoonc01@users.noreply.github.com>
2026-01-16 18:17:55 +0900

    [Silver IV] Title: 스택, Time: 132 ms, Memory: 111660 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+for _ in range(n):
+    cmd = list(input().split())
+    
+    if cmd[0] == "push":
+        stack.append(cmd[1])
+    elif cmd[0] == "pop":
+        print(stack.pop() if stack else -1)
+    elif cmd[0] == "size":
+        print(len(stack))
+    elif cmd[0] == "empty":
+        print(0 if stack else 1)
+    elif cmd[0] == "top":
+        print(stack[-1] if stack else -1)
