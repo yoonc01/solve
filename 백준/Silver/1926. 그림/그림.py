f46f921a8f0924faf64dc3443603fc4eb05a9d65
윤효준 😎 <143938662+yoonc01@users.noreply.github.com>
2026-01-17 20:34:55 +0900

    [Silver I] Title: 그림, Time: 184 ms, Memory: 120192 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,44 +1,36 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 G = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False for _ in range(m)] for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+
+def bfs(sx, sy):
+    q = deque([(sx, sy)])
+    visited[sx][sy] = True
+    area = 1
+
+    dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in dirs:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < m and G[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+                area += 1
+    return area
+
 picture_cnt = 0
 picture_big = 0
 
-def canGo(x, y):
-    return 0 <= x < n and 0 <= y < m and G[x][y] and not visited[x][y]
-
-def bfs(a, b):
-    global picture_big
-    q = deque()
-    big = 1
-    q.append((a, b))
-    visited[a][b] = True
-    dxs = [0, 1, 0, -1]
-    dys = [1, 0, -1, 0]
-
-    while(q):
-        x, y = q.popleft()
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            
-            if canGo(nx, ny):
-                q.append((nx, ny))
-                visited[nx][ny] = True
-                big += 1
-    picture_big = max(picture_big, big)
-    
 for i in range(n):
     for j in range(m):
         if G[i][j] == 1 and not visited[i][j]:
-            bfs(i, j)
             picture_cnt += 1
-            
+            picture_big = max(picture_big, bfs(i, j))
+
 print(picture_cnt)
 print(picture_big)
-
-    
